f888de55a9e7a8714f6e7385739bbf4b29a045cb
Ryota Nakashima <b1615946@planet.kanazawa-it.ac.jp>
2020-11-18 15:19:40 +0900

    add: DefferedRenderingのサンプルプログラムを追加

---
+++ DefferedSampleRenderPipeline.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "DefferedRenderingRenderPipeline.hpp"
+#include "FullscreenTextureRenderPipeline.hpp"
+#include "FullscreenDepthTextureRenderPipeline.hpp"
+#include "DepthPass.hpp"
+
+
+
+
+
+class DefferedSampleRenderPipeline
+	: public DefferedRenderingRenderPipeline
+{
+private:
+	std::shared_ptr<cg::IDepthStencilBuffer> m_shadowMap;
+	DepthPass m_shadowMapRenderingPass;
+private:
+	DefferedSampleRenderPipeline(std::shared_ptr<cg::IRenderTarget> lightingPassRenderTarget, const GeometryPass& geometryPass, std::shared_ptr<cg::ITextureSampler> gbufferSampler, std::shared_ptr<cg::IDepthStencilBuffer> shadowMap);
+	DefferedSampleRenderPipeline(std::shared_ptr<cg::IRenderTarget> lightingPassRenderTarget, std::shared_ptr<cg::IMultipleRenderTarget> geometryPassMRT, std::shared_ptr<cg::IDepthStencilBuffer> geometryPassDepthStencilBuffer, std::shared_ptr<cg::ITextureSampler> gbufferSampler, std::shared_ptr<cg::IDepthStencilBuffer> shadowMap);
+public:
+	DefferedSampleRenderPipeline(std::shared_ptr<cg::IRenderTarget> lightingPassRenderTarget);
+	DefferedSampleRenderPipeline(std::shared_ptr<cg::IRenderTarget> lightingPassRenderTarget, std::shared_ptr<cg::ITextureSampler> gbufferSampler);
+	virtual ~DefferedSampleRenderPipeline() = default;
+
+	void render(const cg::Scene& scene) override;
+	void render() override;
+	
+	void drawImGuiComponents() override;
+};
