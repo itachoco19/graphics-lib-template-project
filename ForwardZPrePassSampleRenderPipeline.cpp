c838c9f5685b7278447e64f2e771a4c6859eaa28
Ryota Nakashima <b1615946@planet.kanazawa-it.ac.jp>
2020-11-20 10:37:36 +0900

    change: ZPrePassForwardRendering -> ForwardZPrePassRenderPipeline

--- ForwardZPrePassSampleRenderPipeline.cpp
+++ ForwardZPrePassSampleRenderPipeline.cpp
@@ -6,6 +6,6 @@
 
 
 ForwardZPrePassSampleRenderPipeline::ForwardZPrePassSampleRenderPipeline(std::shared_ptr<cg::IRenderTarget> renderTarget)
-	: ZPrePassForwardRendering("Z Pre-Pass Forward Rendering", DepthPass("Depth Pass", { std::make_shared<Position3Normal3DepthRenderPipeline>(cg::RasterizationBasedRenderPipeline::TargetRenderingGroupNameList{"SimpleShading"}) }), std::make_shared<ForwardSampleRenderPipeline>(renderTarget, cg::API::shared.graphics()->createDepthStencilTester(cg::ComparisonFunction::equal, cg::ComparisonFunction::always, true, false, true), false, false))
+	: ForwardZPrePassRenderPipeline("Forward Z Pre-Pass Sample Render Pipeline", DepthPass("Depth Pass", { std::make_shared<Position3Normal3DepthRenderPipeline>(cg::RasterizationBasedRenderPipeline::TargetRenderingGroupNameList{"SimpleShading"}) }), std::make_shared<ForwardSampleRenderPipeline>(renderTarget, cg::API::shared.graphics()->createDepthStencilTester(cg::ComparisonFunction::equal, cg::ComparisonFunction::always, true, false, true), false, false))
 {
 }
